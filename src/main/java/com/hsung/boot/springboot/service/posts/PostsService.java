459b24cd3515af865b9207810c1b5ca9a6621cad
heeseong <hsung0823@ipageon.com>
2020-09-14 11:46:31 +0900

    Posts 등록 수정 조회 api 생성

--- src/main/java/com/hsung/boot/springboot/service/posts/PostsService.java
+++ src/main/java/com/hsung/boot/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.hsung.boot.springboot.service.posts;
 
+import com.hsung.boot.springboot.domain.posts.Posts;
 import com.hsung.boot.springboot.domain.posts.PostsRepository;
+import com.hsung.boot.springboot.web.dto.PostsResponseDto;
 import com.hsung.boot.springboot.web.dto.PostsSaveRequestDto;
+import com.hsung.boot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Service;
@@ -15,5 +18,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->  new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 }
